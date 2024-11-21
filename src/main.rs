dc4989542991a098459023be5239dbbd2e6b822b
ilse31 <ilhamprazsetyo33@gmail.com>
2024-11-21 10:19:48 +0700

    looping

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,40 @@ fn main() {
     let string = "test";
     let string = string.len();
     println!("The length of the string is: {}", string);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {}", result);
+
+    let min_type_i32 = i128::MIN;
+
+    println!("The minimum value of i8 is: {}", min_type_i32);
+
+    let pointer123 = &5;
+    println!("The value of pointer123 is: {}", pointer123);
+
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+
+    if let 12 = 5 {
+        println!("The value is 12");
+    } else {
+        println!("The value is not 12");
+    }
+
+    let arraydata = [1, 2, 3, 4, 5];
+    for element in arraydata.iter() {
+        println!("The value of element is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("The value of number is: {}", number);
+    }
 }
