8a875ece8be96c7b67a1b799ff36c31be7dee86d
ilse31 <ilhamprazsetyo33@gmail.com>
2024-11-22 08:59:16 +0700

    update code commit

--- src/main.rs
+++ src/main.rs
@@ -58,4 +58,12 @@ fn main() {
     for number in (1..4).rev() {
         println!("The value of number is: {}", number);
     }
+
+    let a = [10, 20, 30, 40, 50];
+    let slice = &a[1..3];
+    println!("The value of slice is: {}", slice.len());
+
+    let mut v = vec![1, 2, 3];
+    v.push(4);
+    println!("The value of v is: {}", v.len());
 }
